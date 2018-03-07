9faac62340e36e14c5e10411313165649b477918
Nam Trung <namtrung804@gmail.com>
2018-03-07 23:28:17 +0700

    read me

--- README.md
+++ README.md
@@ -1,32 +1,29 @@
 # Requirements
 
-* Windows 7 or higher (Suggest Windows 10)
-* Node js (https://nodejs.org/en/)
+* Nodejs v8 (https://nodejs.org/en/)
 * Yarn (https://yarnpkg.com/en/docs/install)
-* Angularjs 2+
+* Angular 5+
+* Angular CLI v1.6+
+# Function
+- Autocomplete suggestion Place and Near
+- Find Businesses by Place and Near
+- Pagination
+- Google Map Maker
+- Filter Businesses by price, sort by , open now , cash back
+- Responsive website
+- End to end test 
+# Installation instructions
+ ### For Development
+  Run `yarn && yarn start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+ ### For Production
+  Run `yarn build` to build the project. The build artifacts will be stored in the `dist/` directory.  
+ ### Running end-to-end tests
+  Run `yarn e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+  
+# Libraries
+ - lodash (https://lodash.com/)
+ - PrimeNG (https://www.primefaces.org/primeng/#/)
+ - Google Maps for Angular (https://angular-maps.com/)
+ - Yelp API v3 (https://www.yelp.com/developers/documentation/v3)
 
-## Setup and run with local
-    - Clone source from gitlab
-    - Open Command line change directory (`cd`) to project's root directory
-    - Run "yarn && yarn start"
-    - Edit host with "127.0.0.1 m090.dev"
-    - Open brower and run "http://m090.dev:4200" 
-    
-## Edit config ( file "src\app\modules\app.config.ts")
-    Edit username and password API : 
-        - parameter "username","password" 
-    Edit appName call API : 
-        - parameter "appName" 
-    Edit key Captcha : 
-        - parameter "siteKeyCaptcha" 
-    Edit API Url : 
-        - parameter "apiUrl" 
-        
-## Edit Google Analytics key
-    - Open file "src\index.html"
-    - Edit "ga('create','UA-88802472-3', 'auto');"
-    
-## UI Test
-    - Cd to source and run "yarn start"
-    - Open new tab command line "cd" to source and run "yarn e2e"
 
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -16,281 +16,281 @@ import {PageScrollInstance, PageScrollService} from 'ng2-page-scroll';
 import {DOCUMENT} from '@angular/common';
 
 @Component({
-  selector: 'main',
-  templateUrl: 'main.component.html',
-  styleUrls: ['../../../assets/css/search.css', '../../../assets/css/www-pkg.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'main',
+    templateUrl: 'main.component.html',
+    styleUrls: ['../../../assets/css/search.css', '../../../assets/css/www-pkg.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class MainComponent implements OnInit {
-  dataForm: FormGroup;
-  loading = false;
-  validationMessages: any;
-  public boxSearchPlace = new Subject<string>();
-  @ViewChild('inputSearchPlace') inputSearchPlace: any;
-  @ViewChild('inputSearchNear') inputSearchNear: any;
-  listAutocomplete: any = [];
-  isShowListAutocomplete: boolean = false;
-  latitude: number = 0;
-  longitude: number = 0;
-  latitudeTemp: number = 0;
-  longitudeTemp: number = 0;
-  zoom: number = 12;
-  listBusiness: Business[] = [];
-  selectedBusiness: Business;
-  limitPerPage: number = 10;
-  page: number = 0;
-  totalBusiness: number = 10;
-  bigSizeMap: boolean = false;
-  positionInfoWindowTop: number = 0;
-  positionInfoWindowLeft: number = 0;
-  filter: any = {
-    sort_by: 'best_match',
-  }
-  isShowAllFilter: boolean = false;
-
-  constructor(private yelpService: YelpService,
-              private router: Router,
-              private route: ActivatedRoute,
-              private alertService: AlertService,
-              private formBuilder: FormBuilder,
-              private elementRef: ElementRef,
-              private mapsAPILoader: MapsAPILoader,
-              @Inject(DOCUMENT) private document: any,
-              private pageScrollService: PageScrollService,
-              private ngZone: NgZone) {
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    let offset = window.scrollY;
-    let innerWidth = window.innerWidth;
-
-    let map = this.document.getElementById('mapArea');
-    let filterAllArea = this.document.getElementById('filterAllArea');
-    map.style.position = 'relative';
-    if (innerWidth > 998) {
-      if (offset < 300) {
-        map.style.top = `0px`;
-      } else {
-        let maxScroll = this.document.body.scrollHeight - window.innerHeight - this.document.getElementById('footerArea').offsetHeight - filterAllArea.offsetHeight + 300;
-        if (offset > maxScroll) {
-          map.style.top = `${maxScroll}px`;
-        } else {
-          map.style.top = `${offset - 200 - filterAllArea.offsetHeight}px`;
-        }
-      }
-    }else{
-      map.style.top = `0px`;
+    dataForm: FormGroup;
+    loading = false;
+    validationMessages: any;
+    public boxSearchPlace = new Subject<string>();
+    @ViewChild('inputSearchPlace') inputSearchPlace: any;
+    @ViewChild('inputSearchNear') inputSearchNear: any;
+    listAutocomplete: any = [];
+    isShowListAutocomplete: boolean = false;
+    latitude: number = 0;
+    longitude: number = 0;
+    latitudeTemp: number = 0;
+    longitudeTemp: number = 0;
+    zoom: number = 12;
+    listBusiness: Business[] = [];
+    selectedBusiness: Business;
+    limitPerPage: number = 10;
+    page: number = 0;
+    totalBusiness: number = 10;
+    bigSizeMap: boolean = false;
+    positionInfoWindowTop: number = 0;
+    positionInfoWindowLeft: number = 0;
+    filter: any = {
+        sort_by: 'best_match',
     }
-  }
+    isShowAllFilter: boolean = false;
 
-  @HostListener('mousemove', ['$event'])
-  onMousemove(event: MouseEvent) {
-    this.positionInfoWindowTop = event.clientY;
-    this.positionInfoWindowLeft = event.clientX;
-  }
+    constructor(private yelpService: YelpService,
+                private router: Router,
+                private route: ActivatedRoute,
+                private alertService: AlertService,
+                private formBuilder: FormBuilder,
+                private elementRef: ElementRef,
+                private mapsAPILoader: MapsAPILoader,
+                @Inject(DOCUMENT) private document: any,
+                private pageScrollService: PageScrollService,
+                private ngZone: NgZone) {
+    }
 
-  ngOnInit() {
-    // default value
-    this.inputSearchNear.nativeElement.value = 'Hollywood Boulevard, Los Angeles, CA, USA';
-    this.inputSearchPlace.nativeElement.value = 'Pho';
-    // search place
-    this.searchPlace();
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        let offset = window.scrollY;
+        let innerWidth = window.innerWidth;
 
-    //set current position
-    this.setCurrentPosition();
-
-    //load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(this.inputSearchNear.nativeElement, {
-        types: ['address']
-      });
-      autocomplete.addListener('place_changed', () => {
-        this.ngZone.run(() => {
-          //get the place result
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          //verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          //set latitude, longitude and zoom
-          this.latitude = place.geometry.location.lat();
-          this.longitude = place.geometry.location.lng();
-          this.zoom = 12;
-          this.getDataBusiness();
-        });
-      });
-    });
-  }
-
-  searchPlace() {
-    this.boxSearchPlace.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-    ).subscribe((value) => {
-      this.listAutocomplete = [];
-      if (value != '') {
-        this.yelpService.autocomplete(value).subscribe(
-          result => {
-            if (result.categories.length) {
-              for (let key in result.categories) {
-                if (parseInt(key) != 0) {
-                  this.listAutocomplete.push(result.categories[key].title);
+        let map = this.document.getElementById('mapArea');
+        let filterAllArea = this.document.getElementById('filterAllArea');
+        map.style.position = 'relative';
+        if (innerWidth > 998) {
+            if (offset < 300) {
+                map.style.top = `0px`;
+            } else {
+                let maxScroll = this.document.body.scrollHeight - window.innerHeight - this.document.getElementById('footerArea').offsetHeight - filterAllArea.offsetHeight + 300;
+                if (offset > maxScroll) {
+                    map.style.top = `${maxScroll}px`;
+                } else {
+                    map.style.top = `${offset - 200 - filterAllArea.offsetHeight}px`;
                 }
-              }
             }
-            if (result.terms.length) {
-              for (let term of result.terms) {
-                this.listAutocomplete.push(term.text);
-              }
-            }
-          }
-        )
-      }
-    });
-  }
+        } else {
+            map.style.top = `0px`;
+        }
+    }
 
-  setCurrentPosition() {
-    // if (navigator.geolocation) {
-    //   navigator.geolocation.getCurrentPosition((position) => {
-    //       this.latitude = position.coords.latitude;
-    //       this.longitude = position.coords.longitude;
-    //       this.zoom = 12;
-    //       this.mapsAPILoader.load().then(() => {
-    //         let geocoder = new google.maps.Geocoder();
-    //         geocoder.geocode({'location': {lat: this.latitude, lng: this.longitude}}, (results, status) => {
-    //           this.ngZone.run(() => {
-    //             if (status == google.maps.GeocoderStatus.OK) {
-    //               this.inputSearchNear.nativeElement.value = results[0].formatted_address;
-    //               this.getDataBusiness();
-    //             }
-    //           });
-    //         });
-    //       });
-    //     },
-    //     error => {
-    //       this.getDataBusiness();
-    //     });
-    // } else {
-    this.getDataBusiness();
-    // }
-  }
+    @HostListener('mousemove', ['$event'])
+    onMousemove(event: MouseEvent) {
+        this.positionInfoWindowTop = event.clientY;
+        this.positionInfoWindowLeft = event.clientX;
+    }
 
-  redoSearchMap() {
-    this.mapsAPILoader.load().then(() => {
-      let geocoder = new google.maps.Geocoder();
-      geocoder.geocode({'location': {lat: this.latitudeTemp, lng: this.longitudeTemp}}, (results, status) => {
-        this.ngZone.run(() => {
-          if (status == google.maps.GeocoderStatus.OK) {
-            this.inputSearchNear.nativeElement.value = results[0].formatted_address;
-            this.getDataBusiness();
-          }
+    ngOnInit() {
+        // default value
+        this.inputSearchNear.nativeElement.value = 'Hollywood Boulevard, Los Angeles, CA, USA';
+        this.inputSearchPlace.nativeElement.value = 'Pho';
+        // search place
+        this.searchPlace();
+
+        //set current position
+        this.setCurrentPosition();
+
+        //load Places Autocomplete
+        this.mapsAPILoader.load().then(() => {
+            let autocomplete = new google.maps.places.Autocomplete(this.inputSearchNear.nativeElement, {
+                types: ['address']
+            });
+            autocomplete.addListener('place_changed', () => {
+                this.ngZone.run(() => {
+                    //get the place result
+                    let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+                    //verify result
+                    if (place.geometry === undefined || place.geometry === null) {
+                        return;
+                    }
+
+                    //set latitude, longitude and zoom
+                    this.latitude = place.geometry.location.lat();
+                    this.longitude = place.geometry.location.lng();
+                    this.zoom = 12;
+                    this.getDataBusiness();
+                });
+            });
         });
-      });
-    });
-  }
+    }
 
-  centerMapChange(event: any) {
-    this.latitudeTemp = event.lat;
-    this.longitudeTemp = event.lng;
-  }
+    searchPlace() {
+        this.boxSearchPlace.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+        ).subscribe((value) => {
+            this.listAutocomplete = [];
+            if (value != '') {
+                this.yelpService.autocomplete(value).subscribe(
+                    result => {
+                        if (result.categories.length) {
+                            for (let key in result.categories) {
+                                if (parseInt(key) != 0) {
+                                    this.listAutocomplete.push(result.categories[key].title);
+                                }
+                            }
+                        }
+                        if (result.terms.length) {
+                            for (let term of result.terms) {
+                                this.listAutocomplete.push(term.text);
+                            }
+                        }
+                    }
+                )
+            }
+        });
+    }
 
-  showInfoWindowBusinessMap(business: Business) {
-    this.selectedBusiness = business;
-    let infoWindow = this.document.getElementById('infoWindowBusiness');
-    infoWindow.style.top = `${this.positionInfoWindowTop - infoWindow.offsetHeight}px`;
-    infoWindow.style.left = `${this.positionInfoWindowLeft - infoWindow.offsetWidth}px`;
-  }
-
-  hideInfoWindowBusinessMap() {
-    let infoWindow = this.document.getElementById('infoWindowBusiness');
-    infoWindow.style.top = `-9999px`;
-    infoWindow.style.left = `-9999px`;
-  }
-
-  chosePlace(text: string) {
-    this.inputSearchPlace.nativeElement.value = text;
-    this.isShowListAutocomplete = false;
-    this.getDataBusiness();
-  }
-
-  addFilter(key: string, value: string) {
-    if (_.isUndefined(this.filter[key])) {
-      // underfined
-      if (key == 'price' || key == 'attributes') {
-        this.filter[key] = [];
-        this.filter[key].push(value);
-      } else {
-        this.filter[key] = value;
-      }
-    } else {
-      if (key == 'price' || key == 'attributes') {
-        if (this.filter[key].indexOf(value) == -1) {
-          // not yet value
-          this.filter[key].push(value);
+    setCurrentPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                    this.latitude = position.coords.latitude;
+                    this.longitude = position.coords.longitude;
+                    this.zoom = 12;
+                    this.mapsAPILoader.load().then(() => {
+                        let geocoder = new google.maps.Geocoder();
+                        geocoder.geocode({'location': {lat: this.latitude, lng: this.longitude}}, (results, status) => {
+                            this.ngZone.run(() => {
+                                if (status == google.maps.GeocoderStatus.OK) {
+                                    this.inputSearchNear.nativeElement.value = results[0].formatted_address;
+                                    this.getDataBusiness();
+                                }
+                            });
+                        });
+                    });
+                },
+                error => {
+                    this.getDataBusiness();
+                });
         } else {
-          this.filter[key] = this.filter[key].filter((item: any, index: any) => item != value);
+            this.getDataBusiness();
         }
-      } else {
-        if (value == this.filter[key]) {
-          delete this.filter[key];
+    }
+
+    redoSearchMap() {
+        this.mapsAPILoader.load().then(() => {
+            let geocoder = new google.maps.Geocoder();
+            geocoder.geocode({'location': {lat: this.latitudeTemp, lng: this.longitudeTemp}}, (results, status) => {
+                this.ngZone.run(() => {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        this.inputSearchNear.nativeElement.value = results[0].formatted_address;
+                        this.getDataBusiness();
+                    }
+                });
+            });
+        });
+    }
+
+    centerMapChange(event: any) {
+        this.latitudeTemp = event.lat;
+        this.longitudeTemp = event.lng;
+    }
+
+    showInfoWindowBusinessMap(business: Business) {
+        this.selectedBusiness = business;
+        let infoWindow = this.document.getElementById('infoWindowBusiness');
+        infoWindow.style.top = `${this.positionInfoWindowTop - infoWindow.offsetHeight}px`;
+        infoWindow.style.left = `${this.positionInfoWindowLeft - infoWindow.offsetWidth}px`;
+    }
+
+    hideInfoWindowBusinessMap() {
+        let infoWindow = this.document.getElementById('infoWindowBusiness');
+        infoWindow.style.top = `-9999px`;
+        infoWindow.style.left = `-9999px`;
+    }
+
+    chosePlace(text: string) {
+        this.inputSearchPlace.nativeElement.value = text;
+        this.isShowListAutocomplete = false;
+        this.getDataBusiness();
+    }
+
+    addFilter(key: string, value: string) {
+        if (_.isUndefined(this.filter[key])) {
+            // underfined
+            if (key == 'price' || key == 'attributes') {
+                this.filter[key] = [];
+                this.filter[key].push(value);
+            } else {
+                this.filter[key] = value;
+            }
         } else {
-          this.filter[key] = value;
+            if (key == 'price' || key == 'attributes') {
+                if (this.filter[key].indexOf(value) == -1) {
+                    // not yet value
+                    this.filter[key].push(value);
+                } else {
+                    this.filter[key] = this.filter[key].filter((item: any, index: any) => item != value);
+                }
+            } else {
+                if (value == this.filter[key]) {
+                    delete this.filter[key];
+                } else {
+                    this.filter[key] = value;
+                }
+
+            }
         }
-
-      }
+        this.getDataBusiness();
     }
-    this.getDataBusiness();
-  }
 
-  getDataBusiness(page: number = 0) {
-    this.page = page;
-    this.loading = true;
-    this.totalBusiness = 0;
-    this.listBusiness = [];
-    let dataSearch = {
-      term: this.inputSearchPlace.nativeElement.value,
-      location: this.inputSearchNear.nativeElement.value,
-      limit: this.limitPerPage,
-      offset: page * this.limitPerPage
-    }
-    dataSearch = _.merge(dataSearch, this.filter);
-    this.yelpService.getDataBusinessSearch(dataSearch).subscribe(
-      result => {
-        this.loading = false;
-        if (result.businesses.length) {
-          this.listBusiness = result.businesses;
-          this.totalBusiness = result.total;
-          this.latitude = result.region.center.latitude;
-          this.longitude = result.region.center.longitude;
-          this.zoom = 12;
+    getDataBusiness(page: number = 0) {
+        this.page = page;
+        this.loading = true;
+        this.totalBusiness = 0;
+        this.listBusiness = [];
+        let dataSearch = {
+            term: this.inputSearchPlace.nativeElement.value,
+            location: this.inputSearchNear.nativeElement.value,
+            limit: this.limitPerPage,
+            offset: page * this.limitPerPage
         }
-        this.scrollToTop();
-      },
-      error => {
-        this.loading = false;
-      }
-    )
+        dataSearch = _.merge(dataSearch, this.filter);
+        this.yelpService.getDataBusinessSearch(dataSearch).subscribe(
+            result => {
+                this.loading = false;
+                if (result.businesses.length) {
+                    this.listBusiness = result.businesses;
+                    this.totalBusiness = result.total;
+                    this.latitude = result.region.center.latitude;
+                    this.longitude = result.region.center.longitude;
+                    this.zoom = 12;
+                }
+                this.scrollToTop();
+            },
+            error => {
+                this.loading = false;
+            }
+        )
 
-  }
-
-  getAddressShop(address: any) {
-    if (address != null && address.length) {
-      return address.join('<br>');
     }
-    return '';
-  }
 
-  getNumberShop(num: number) {
-    let position = this.page * this.limitPerPage + num + 1;
-    return position.toString();
-  }
+    getAddressShop(address: any) {
+        if (address != null && address.length) {
+            return address.join('<br>');
+        }
+        return '';
+    }
 
-  scrollToTop() {
-    let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, 'body');
-    this.pageScrollService.start(pageScrollInstance);
-  }
+    getNumberShop(num: number) {
+        let position = this.page * this.limitPerPage + num + 1;
+        return position.toString();
+    }
+
+    scrollToTop() {
+        let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, 'body');
+        this.pageScrollService.start(pageScrollInstance);
+    }
 
 }
